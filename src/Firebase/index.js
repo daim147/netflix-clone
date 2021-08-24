bfad659606f9249a1d44c12fe94525b82cf7f36e
DaimSyed <daimshah784@gmail.com>
2021-08-24 16:44:50 +0500

    added

---
+++ src/Firebase/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAy-2LhGFz919Z39wQ-Xopm1K_F4PXyzeo",
+  authDomain: "netflix-clone-fd337.firebaseapp.com",
+  projectId: "netflix-clone-fd337",
+  storageBucket: "netflix-clone-fd337.appspot.com",
+  messagingSenderId: "758868226941",
+  appId: "1:758868226941:web:57451acc416e55c0115214",
+};
